d0d795b7a6877136c6240d537eac4abec39941e7
kevin <wei.liu@ifchange.com>
2020-05-29 10:51:51 +0800

    visiable MapperScan

--- src/main/java/com/klbud/discover/DiscoverApplication.java
+++ src/main/java/com/klbud/discover/DiscoverApplication.java
@@ -1,14 +1,14 @@
 package com.klbud.discover;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.example.demo.dao.*")
+@MapperScan("com.klbud.discover.dao.*")
 public class DiscoverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoverApplication.class, args);
     }
-
 }
